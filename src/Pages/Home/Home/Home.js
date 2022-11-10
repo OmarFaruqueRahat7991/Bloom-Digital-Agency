32a57a488dd851676a9b364ca3de2425ba72bf45
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-11-10 06:34:36 +0600

    another commit

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import About from './About/About';
+import AboutMe from './AboutMe/AboutMe';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <AboutMe></AboutMe>
         </div>
     );
 };
